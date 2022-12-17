b430196e84a3bb21c1d3717d76e460711152f0bc
LauricellaAndrea <andrealauricella97@gmail.com>
2022-12-17 18:41:26 +0100

    add-es

--- Esercizio-05/CallName.mjs
+++
@@ -1,5 +0,0 @@
-
-import setName from "./Name.mjs"; //mjs è l'estensione di riferimento per un file ECMAscript
-
-setName("Gennarino");
-
--- Esercizio-05/Name.mjs
+++
@@ -1,5 +0,0 @@
-function setName(name) {
-  console.log(`Welcome ${name}!`);
-}
-
-export default setName // con il sistema ECMAScript è di utilizzo standard utilizzare import e export
